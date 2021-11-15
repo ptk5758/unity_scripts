b4320b7cf2a614a9b31bb6a538791f9e95aefcd6
박태광 <ptk725739@gmail.com>
2021-11-15 21:30:57 +0900

    Player Class Particle Event Add commit

--- Player.cs
+++ Player.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int money; // 현재소지금
+    public Text moneyUI; // 현재소지금 UI
+    public int moneyPower; // 머니 파워
+
+    public bool isFever; //피버 인지아닌지
+    public Text FeverTimeUi; // 피버 타임 UI
+    public float feverTime; // 피버 타임
+
+    public ParticleSystem feverEffect; // 피버 이벤트    
+
+    private void Update()
     {
-        
+        moneyUI.text = "지갑 : "+ this.money +" 원";
+        FeverEvent();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddMoney()
     {
-        
+        int mp = isFever ? moneyPower * 2 : moneyPower;
+        this.money = this.money + mp;
+    }
+
+    public void ToggleFever()
+    {
+        isFever = !isFever;
+        //Instantiate(feverEffect, FeverTimeUi.transform);
+        ParticleSystem particleObject = Instantiate(feverEffect, FeverTimeUi.transform);
+        particleObject.Play();
+    }
+
+    private void FeverEvent()
+    {
+        if (feverTime <= 0) {
+            isFever = false;
+        }
+        if (isFever) {
+            feverTime -= Time.deltaTime;
+        }
+        FeverTimeUi.text = Mathf.Round(feverTime) + "초";
     }
 }
